f8a0351220da20a542a458b74eb9592c53862cc1
Marin Gerov <marin.gerov@gmail.com>
2015-07-30 23:07:50 +0200

    Added first proper components and dummy state to display notes

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import Note from './Note';
+import Notes from './Notes';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.addItem = this.addItem.bind(this);
+
+    this.state = {
+      notes: [
+        {
+          task: 'Learn webpack'
+        },
+        {
+          task: 'Learn react'
+        },
+        {
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+
   render() {
-    return <Note />;
+    const notes = this.state.notes;
+
+    return (
+      <div>
+        <button onClick={this.addItem}>+</button>
+        <Notes items={notes} />
+      </div>
+    );
+  }
+
+  addItem() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        task: 'new task'
+      }])
+    });
   }
 }
---
+++ app/components/Notes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Note from './Note';
+
+export default class Notes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderNote = this.renderNote.bind(this);
+  }
+
+  render() {
+    const notes = this.props.items
+    return (
+      <ul className='notes'>{notes.map(this.renderNote)}</ul>
+    );
+  }
+
+  renderNote(note, i) {
+    return (
+      <li className='note' key={`note${i}`}>
+        <Note value={note.task} />
+      </li>
+    );
+  }
+}
